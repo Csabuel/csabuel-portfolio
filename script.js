29ab2c14d0b44cbfc9733a7c1bea80bbd96bd3fa
Csabuel <119868774+Csabuel@users.noreply.github.com>
2023-06-22 13:04:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const pictures = [
   [
-    '/Project pict/Project1 pic 1.png',
-    '/Project pict/Project1 pic 2.png',
-    '/Project pict/Project1 pic 3.png'
+    'Project pict/Project1 pic 1.png',
+    'Project pict/Project1 pic 2.png',
+    'Project pict/Project1 pic 3.png'
   ],
   [
-    '/Project pict/Project3 pic 1.png',
-    '/Project pict/Project3 pic 2.png',
-    '/Project pict/Project3 pic 3.png'
+    'Project pict/Project3 pic 1.png',
+    'Project pict/Project3 pic 2.png',
+    'Project pict/Project3 pic 3.png'
   ],
   [
-    '/Project pict/Project2 pic 1.png',
-    '/Project pict/Project2 pic 2.png',
-    '/Project pict/Project2 pic 3.png'
+    'Project pict/Project2 pic 1.png',
+    'Project pict/Project2 pic 2.png',
+    'Project pict/Project2 pic 3.png'
   ]
 ];
 
